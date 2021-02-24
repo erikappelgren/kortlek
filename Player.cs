188e1fba01e1763bf5da4a4d5ddacf321d64e69b
Erik <erik.appelgren@hotmail.com>
2021-02-24 12:07:11 +0100

    Added Deal osv

--- Player.cs
+++ Player.cs
@@ -9,10 +9,11 @@ namespace OOP_kortlek
       
         public int Points  { get; set; }
         public string Name { get; set; }
+        public List<Player> users { get; set; }
 
         public Player()
         {
-
+            users = new List<Player>();
         }
         public Player(int points, string name)
         {
--- Program.cs
+++ Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using static System.Console;
 using System.Collections.Generic;
+
 namespace OOP_kortlek
 {
     class Program
     {
+        //public List<Player> users = new List<Player>();
         static void Main(string[] args)
         {
-            Welcome();
-            Console.WriteLine("Hello World!");
-            
+            //Welcome();
+            Deal();
         }
         static void Welcome()
         {
-            List<Player> users = new List<Player>();
+            //List<Player> users = new List<Player>();
             int playerCount = 0;
             WriteLine("Welcome To card 21");
             WriteLine("How manny players are there? ");
@@ -31,8 +32,30 @@ namespace OOP_kortlek
                 Console.WriteLine($"What is player {i} name?");
                 string playername = Console.ReadLine();
                 Player obj = new Player(100, playername);
-                users.Add(obj);
+                //Player.users.Add(obj); FIXA
             }
+        }
+        static void Deal()
+        {
+            List<int> listOfCards = new List<int>();
+            for(int i = 1; i < 14; i++)
+            {
+                listOfCards.Add(i);
+                listOfCards.Add(i);
+                listOfCards.Add(i);
+                listOfCards.Add(i);
+            }
+            //foreach(int card in listOfCards)
+            //{
+            //    Console.WriteLine(card);
+            //}
+            Random rnd = new Random();
+            int index = rnd.Next(1, listOfCards.Count);
+            index -= 1;
+            int cardNumber = listOfCards[index];
+            listOfCards.Remove(index);
+
+            
         }
     }
     
