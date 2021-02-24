58734ff528733e3f445fb20c89c4cab7248d78c9
KonstigSwe <swekonstig@gmail.com>
2021-02-24 11:31:18 +0100

    Created welcome

---
+++ Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_kortlek
+{
+    class Player
+    {
+      
+        public int Points  { get; set; }
+        public string Name { get; set; }
+
+        public Player()
+        {
+
+        }
+        public Player(int points, string name)
+        {
+            Points = points;
+            Name = name;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-
+using static System.Console;
+using System.Collections.Generic;
 namespace OOP_kortlek
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Welcome();
             Console.WriteLine("Hello World!");
+            
+        }
+        static void Welcome()
+        {
+            List<Player> users = new List<Player>();
+            int playerCount = 0;
+            WriteLine("Welcome To card 21");
+            WriteLine("How manny players are there? ");
+            try
+            {
+              playerCount  = int.Parse(Console.ReadLine());
+            }
+            catch {
+                Console.WriteLine("you fucked up");
+                Welcome(); 
+            }
+           
+            for (int i = 0; i < playerCount; i++)
+            {
+                Console.WriteLine($"What is player {i} name?");
+                string playername = Console.ReadLine();
+                Player obj = new Player(100, playername);
+                users.Add(obj);
+            }
         }
     }
+    
 }
