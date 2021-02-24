4fbf5639b5fe8fa20d512a0d4deb801949fbbc75
Erik <erik.appelgren@hotmail.com>
2021-02-24 09:39:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_kortlek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
